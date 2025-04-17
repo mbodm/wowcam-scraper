acc75c8bdae355559d495e5fff658ed405bde26d
MBODM <webmaster@mbodm.com>
2025-04-17 15:06:27 +0200

    Fixed a memory leak bug (forgot the try/finally to make sure Puppeteer browser is closed).

--- release/out.cjs
+++ release/out.cjs
@@ -83635,51 +83635,56 @@ function createSuccess(result) {
 async function parseSite(addonSlug) {
   let browser;
   try {
-    browser = await launch3({ headless: true });
-  } catch (e) {
-    console.log(e);
-    return createError("Puppeteer exception occurreed while launching browser.");
-  }
-  let page;
-  try {
-    page = await browser.newPage();
-    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-    await page.setExtraHTTPHeaders({ "Accept-Language": "en" });
-  } catch (e) {
-    console.log(e);
-    return createError(e, "Puppeteer exception occurreed while getting page.");
-  }
-  const url = `http://www.curseforge.com/wow/addons/${addonSlug}`;
-  try {
-    const response = await page.goto(url, { waitUntil: "load", timeout: 3e4 });
-    const status = response.status();
-    if (status !== 200) {
-      if (status === 404) {
-        return createError("It seems like the addon page does not exist (this usually happens when the given addon param is not a valid addon slug).");
+    try {
+      browser = await launch3({ headless: true });
+    } catch (e) {
+      console.log(e);
+      return createError("Puppeteer exception occurreed while launching browser.");
+    }
+    let page;
+    try {
+      page = await browser.newPage();
+      await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+      await page.setExtraHTTPHeaders({ "Accept-Language": "en" });
+    } catch (e) {
+      console.log(e);
+      return createError(e, "Puppeteer exception occurreed while getting page.");
+    }
+    const url = `http://www.curseforge.com/wow/addons/${addonSlug}`;
+    try {
+      const response = await page.goto(url, { waitUntil: "load", timeout: 3e4 });
+      const status = response.status();
+      if (status !== 200) {
+        if (status === 404) {
+          return createError("It seems like the addon page does not exist (this usually happens when the given addon param is not a valid addon slug).");
+        }
+        return createError("Puppeteer reponse error occurred while going to page, but the response status code was not HTTP 404 (which is rather unexpected).");
       }
-      return createError("Puppeteer reponse error occurred while going to page, but the response status code was not HTTP 404 (which is rather unexpected).");
+    } catch (e) {
+      console.log(e);
+      return createError("Puppeteer exception occurreed while going to page.");
     }
-  } catch (e) {
-    console.log(e);
-    return createError("Puppeteer exception occurreed while going to page.");
+    let json;
+    try {
+      const element = await page.$("script#__NEXT_DATA__");
+      if (!element) {
+        return createError("Could not found JSON script element in page.");
+      }
+      const text = await page.evaluate((el) => el.textContent, element);
+      if (!text) {
+        return createError("Found JSON script element in page, but the element was empty.");
+      }
+      json = text;
+    } catch (e) {
+      console.log(e);
+      return createError("Puppeteer exception occurreed while evaluating page.");
+    }
+    return createSuccess(json);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
-  let json;
-  try {
-    const element = await page.$("script#__NEXT_DATA__");
-    if (!element) {
-      return createError("Could not found JSON script element in page.");
-    }
-    const text = await page.evaluate((el) => el.textContent, element);
-    if (!text) {
-      return createError("Found JSON script element in page, but the element was empty.");
-    }
-    json = text;
-  } catch (e) {
-    console.log(e);
-    return createError("Puppeteer exception occurreed while evaluating page.");
-  }
-  await browser.close();
-  return createSuccess(json);
 }
 
 // src/curse.js
